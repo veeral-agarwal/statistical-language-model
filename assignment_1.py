f84d77c8f7d211e3493daf2a084d607df37ba75b
veeral-agarwal <veeral.agarwal@research.iiit.ac.in>
2021-06-23 17:28:06 +0530

    some minor changes

---
+++ assignment_1.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+from string import punctuation as punkts
+import re
+
+file = open('brown.txt')
+data = file.read()
+data = data.split('.')
+
+
+# In[7]:
+
+
+import re
+def clean(string):
+    string = string.encode('ascii', 'ignore').decode('ascii')
+    string = re.sub('^\[.*\]','',string)
+    string = re.sub('^.*:','',string)
+    string = string.strip()
+    string = string.strip('\"')
+    string = re.sub('\?+','?',string)
+    string = re.sub('!+','!',string)
+    string = re.sub('\.+','.',string)
+    string = re.sub(' +',' ',string)
+    string = re.sub('\(.*\)','',string)
+    string = string.replace(':)','')
+    string = string.replace(';)','')
+    string = string.replace('*','')
+    string = string.strip()
+    return string
+
+
+# In[8]:
+
+
+for i in range(len(data)):
+    data[i] = data[i].replace('\n',' ')
+    data[i] = clean(data[i])
+    data[i] = data[i] + '.'
+cleaned = []
+for i in range(len(data)):
+    if len(data[i])>=10:
+        cleaned.append(data[i])
+
+
+# In[9]:
+
+
+len(cleaned)
+
+
+# In[10]:
+
+
+train_data = cleaned[:35000]
+valid_data = cleaned[35000:40000]
+test_data = cleaned[40000:]
+
+
+# In[11]:
+
+
+def tokenize(data):
+    # tokenizes and returns a list of strings
+    new_data = []
+    for line in data:
+        temp_line = ' '
+        temp_line = line.lower()
+        for x in punkts:
+            temp_line = temp_line.replace(x,f' {x}')
+        #new_line = re.sub(' +', ' ', new_line)
+        temp_line = temp_line.strip()
+        new_line = temp_line.split(' ')
+        new_line = ['<s>']*3 + new_line + ['</s>']
+        new_data.append(new_line)
+    return new_data
+
+train_data = tokenize(train_data)
+valid_data = tokenize(valid_data)
+test_data = tokenize(test_data)
+
+total_words = 0
+for line in train_data:
+    for word in line:
+        total_words += 1
+
+def ngrams_generator(data):
+    ngrams = [{} for i in range(5)]
+    for n in [1,2,3,4]:
+        for line in data:
+            for i in range(len(line)-n):
+                temp_gram = line[i:i+n]
+                temp_gram_str = ' '.join(temp_gram)
+                if temp_gram_str in ngrams[n]:
+                    ngrams[n][temp_gram_str] += 1
+                else:
+                    ngrams[n][temp_gram_str] = 1
+    return ngrams
+
+ngrams = ngrams_generator(train_data)
+
+def word_types(context, n):
+    count = 0
+    for words in ngrams[n].keys():
+        if words.startswith(context):
+            count+=1
+    return count
+
+totalent = [0]*5
+for i in range(5):
+    totalent[i] = sum(ngrams[i].values())
+
+def knesernay(sent):
+    d = 0.75
+    p = 1
+    def do_calculations(i,n):
+        context = ' '.join(sent[i-n+1:i])
+        new_ngram = ' '.join(sent[i-n+1:i+1])
+        if n == 1:
+            if new_ngram in ngrams[n]:
+                first_num = max(ngrams[n][new_ngram] - d, 0)
+                first_dem = total_words
+                first_elem = first_num/first_dem
+            else:
+                first_elem = 0
+            second_elem = d/len(ngrams[n])
+            return first_elem + second_elem
+        if new_ngram in ngrams[n]:
+            first_num = max(ngrams[n][new_ngram] - d, 0)
+            first_dem = ngrams[n-1][context]
+            first_elem = first_num/first_dem
+        else:
+            first_elem = 0
+        if context in ngrams[n-1]:
+            interpolator = (d/ngrams[n-1][context])*word_types(context, n)
+            interpolated = do_calculations(i,n-1)
+            second_elem = interpolator * interpolated
+        else:
+            interpolator = ((d*len(ngrams[n-1]))/totalent[n-1])
+            interpolated = do_calculations(i,n-1)
+            second_elem = interpolator * interpolated
+        return first_elem + second_elem
+    for i in range(3,len(sent)-1):
+        p *= do_calculations(i,4)
+    return (p ** (-1/len(sent)))
+
+def wittenbell(sent):
+    d = 0.75
+    p = 1
+    def do_calculations(i,n):
+        context = ' '.join(sent[i-n+1:i])
+        new_ngram = ' '.join(sent[i-n+1:i+1])
+        if n == 1:
+            if new_ngram in ngrams[n]:
+                first_num = ngrams[n][new_ngram]
+                first_dem = total_words
+                first_elem = first_num/first_dem
+            else:
+                first_elem = 0
+            second_elem = d/len(ngrams[n])
+            return first_elem + second_elem
+        elif new_ngram in ngrams[n]:
+            first_elem = ngrams[n][new_ngram]
+        else:
+            first_elem = 0
+        if context in ngrams[n-1]:
+            nplus = word_types(context, n)
+            second_elem = ngrams[n-1][context]
+        else:
+            nplus = do_calculations(i,n-1)
+            second_elem = ((d*len(ngrams[n-1]))/totalent[n-1])
+        return (first_elem + nplus*do_calculations(i,n-1))/(second_elem + nplus)
+    for i in range(3,len(sent)-1):
+        p *= do_calculations(i,4)
+    return (p ** (-1/len(sent)))
+
+
+
+# In[12]:
+
+
+output_name = '2019114004-Witten-Valid-Perplexity.txt'
+f = open(output_name,'w')
+tot, c = 0, 0
+for sent in valid_data[:100]:
+    try:
+        val = wittenbell(sent)
+        tot += val
+        c += 1
+        print(c)
+        if c%10==0:
+            print(val)
+        temp = ' '.join(sent)+'\t'+str(val)+'\n'
+        f.write(temp)
+    except:
+        print('uh')
+        
+avg = tot/c
+temp = "Average"+'\t'+str(avg)+'\n'
+f.write(temp)
+f.close()
+
+
--- main.py
+++ main.py
@@ -1,67 +1,186 @@
-import stanza
+# importing relavent libraries
+from string import punctuation as punkts
+import re
 
 
-def get_dependencies(doc, n):
-    """Get dependencies in the format of a list of
-    (token, deprel, dependent_token) pairs- 
-    for all 'n' sentences in doc"""
+# loading data
+file = open('brown.txt')# data file in argv[1]
+data = file.read()
+data = data.split('.')
 
-    def getdeps(i):
-        deps = []
-        for head, rel, dep in doc.sentences[i].dependencies:
-            deps.append((head.text, rel, dep.text))
-        return deps
+#data cleaning 
+import re
+def clean(string):
+    string = string.encode('ascii', 'ignore').decode('ascii')
+    string = re.sub('^\[.*\]','',string)
+    string = re.sub('^.*:','',string)
+    string = string.strip()
+    string = string.strip('\"')
+    string = re.sub('\?+','?',string)
+    string = re.sub('!+','!',string)
+    string = re.sub('\.+','.',string)
+    string = re.sub(' +',' ',string)
+    string = re.sub('\(.*\)','',string)
+    string = string.replace(':)','')
+    string = string.replace(';)','')
+    string = string.replace('*','')
+    string = string.strip()
+    return string
 
-    return [getdeps(i) for i in range(n)]
 
+#cleaned data generation
+for i in range(len(data)):
+    data[i] = data[i].replace('\n',' ')
+    data[i] = clean(data[i])
+    data[i] = data[i] + '.'
+cleaned = []
+for i in range(len(data)):
+    if len(data[i])>=10:
+        cleaned.append(data[i])
 
-def get_pos_tags(doc, n):
-    """Get POS-tagged tokens in the format of a list of 
-    (token, POStag) pairs for all sentences in doc.
-    Returns upos (Universal part-of-speech) tag only, not 
-    xpos (treebank-specific part of speech)"""
+#train tese split
+train_data = cleaned[:35000]
+valid_data = cleaned[35000:40000]
+test_data = cleaned[40000:]
 
-    def getpos(i):
-        tokens = []
-        for token in doc.sentences[i].words:
-            tokens.append((token.text, token.upos))
-        return tokens
+#tokenization (returns a list of strings) 
+def tokenize(data):
+    # tokenizes and returns a list of strings
+    new_data = []
+    for line in data:
+        temp_line = ' '
+        temp_line = line.lower()
+        for x in punkts:
+            temp_line = temp_line.replace(x,f' {x}')
+        #new_line = re.sub(' +', ' ', new_line)
+        temp_line = temp_line.strip()
+        new_line = temp_line.split(' ')
+        new_line = ['<s>']*3 + new_line + ['</s>']
+        new_data.append(new_line)
+    return new_data
 
-    return [getpos(i) for i in range(n)]
+#tokenization of train test and validation 
+train_data = tokenize(train_data)
+valid_data = tokenize(valid_data)
+test_data = tokenize(test_data)
 
-# ==== MAIN: English ====
-# stanza.download('en')
-nlp_en = stanza.Pipeline('en') # This sets up a default neural pipeline in English
-doc = nlp_en("Barack Obama was born in Hawaii.  He was elected president in 2008.")
+#counting total words
+total_words = 0
+for line in train_data:
+    for word in line: 
+        total_words += 1
 
-# this prints the dependencies in a human-readable format
-print(get_dependencies(doc, 2))
+#creating n-grams
+def ngrams_generator(data):
+    ngrams = [{} for i in range(5)]
+    for n in [1,2,3,4]:
+        for line in data:
+            for i in range(len(line)-n):
+                temp_gram = line[i:i+n]
+                temp_gram_str = ' '.join(temp_gram)
+                if temp_gram_str in ngrams[n]:
+                    ngrams[n][temp_gram_str] += 1
+                else:
+                    ngrams[n][temp_gram_str] = 1
+    return ngrams
 
-# this prints the POS-tagged sentence in a human-readable format
-print(get_pos_tags(doc, 2))
+ngrams = ngrams_generator(train_data)
 
-# ==== MAIN: Hindi ====
+def word_types(context, n):
+    count = 0
+    for words in ngrams[n].keys():
+        if words.startswith(context):
+            count+=1
+    return count
 
-# this sets up a default neural pipeline for Hindi
-# stanza.download('hi')
-nlp_hi = stanza.Pipeline('hi')
-doc_hi = nlp_hi("नमस्ते तुम कैसे हो")
+totalent = [0]*5
+for i in range(5):
+    totalent[i] = sum(ngrams[i].values())
 
-# this accesses the list of Token objects in the sentence
-# for more on the data structures in a Document, see here:
-# https://stanfordnlp.github.io/stanza/data_objects.html
+#kneser
+def kneser(sent):
+    d = 0.75
+    p = 1
+    def do_calculations(i,n):
+        context = ' '.join(sent[i-n+1:i])
+        new_ngram = ' '.join(sent[i-n+1:i+1])
+        if n == 1:
+            if new_ngram in ngrams[n]:
+                first_num = max(ngrams[n][new_ngram] - d, 0)
+                first_dem = total_words
+                first_elem = first_num/first_dem
+            else:
+                first_elem = 0
+            second_elem = d/len(ngrams[n])
+            return first_elem + second_elem
+        if new_ngram in ngrams[n]:
+            first_num = max(ngrams[n][new_ngram] - d, 0)
+            first_dem = ngrams[n-1][context]
+            first_elem = first_num/first_dem
+        else:
+            first_elem = 0
+        if context in ngrams[n-1]:
+            interpolator = (d/ngrams[n-1][context])*word_types(context, n)
+            interpolated = do_calculations(i,n-1)
+            second_elem = interpolator * interpolated
+        else:
+            interpolator = ((d*len(ngrams[n-1]))/totalent[n-1])
+            interpolated = do_calculations(i,n-1)
+            second_elem = interpolator * interpolated
+        return first_elem + second_elem
+    for i in range(3,len(sent)-1):
+        p *= do_calculations(i,4)
+    return (p ** (-1/len(sent)))
 
-#
-print(doc_hi.sentences[0].tokens)
-#
+#wittenbell
+def wittenbell(sent):
+    d = 0.75
+    p = 1
+    def do_calculations(i,n):
+        context = ' '.join(sent[i-n+1:i])
+        new_ngram = ' '.join(sent[i-n+1:i+1])
+        if n == 1:
+            if new_ngram in ngrams[n]:
+                first_num = ngrams[n][new_ngram]
+                first_dem = total_words
+                first_elem = first_num/first_dem
+            else:
+                first_elem = 0
+            second_elem = d/len(ngrams[n])
+            return first_elem + second_elem
+        elif new_ngram in ngrams[n]:
+            first_elem = ngrams[n][new_ngram]
+        else:
+            first_elem = 0
+        if context in ngrams[n-1]:
+            nplus = word_types(context, n)
+            second_elem = ngrams[n-1][context]
+        else:
+            nplus = do_calculations(i,n-1)
+            second_elem = ((d*len(ngrams[n-1]))/totalent[n-1])
+        return (first_elem + nplus*do_calculations(i,n-1))/(second_elem + nplus)
+    for i in range(3,len(sent)-1):
+        p *= do_calculations(i,4)
+    return (p ** (-1/len(sent)))
 
-# this prints the dependency tree in a human-readable format
-
-#
-doc_hi.sentences[0].print_dependencies()
-#
-
-# same set of functions as above- but for Hindi
-print(get_dependencies(doc_hi, 1))
-print(get_pos_tags(doc_hi, 1))
-
+# enter outout file 
+output_name = 'temp.txt' # change output filename here 
+f = open(output_name,'w')
+tot, c = 0, 0
+for sent in valid_data[:100]:
+    try:
+        val = wittenbell(sent) # or kneser
+        tot += val
+        c += 1
+        print(c)
+        if c%10==0:
+            print(val)
+        temp = ' '.join(sent)+'\t'+str(val)+'\n'
+        f.write(temp)
+    except:
+        print('f')
+        
+avg = tot/c
+temp = "Average"+'\t'+str(avg)+'\n'
+f.write(temp)
+f.close()
